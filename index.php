6ee4d0d4a182e31c5ebff03fcfcd4911b37bf0b8
csc354eval <50844891+csc354eval@users.noreply.github.com>
2019-05-20 15:03:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,36 +4,34 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="style.css">
     <title>Capstone Evaluation</title>
-    <script src="code.js"></script>
   </head>
   <body>
 <?php
-//        session_start();
-//
-//      if (isset($_SESSION))
-//        {
-//                print"<nav>";
-//                print"<a href=\"log_out.php\">Log Out</a>";
-//                print" ";
-//                print"<a href=\"rubric.php\">Rubric</a>";
-//                print"</nav>";
-//        }
-//       else
-//        {
-//                print"<nav>";
-//                print"<a href=\"sign_in.php\">Sign In</a>";
-//                print" ";
-//                print"<a href=\"rubric.php\">Rubric</a>";
-//                print"</nav>";
-//        }
+        session_start();
+
+      if (isset($_SESSION))
+        {
+                print"<nav>";
+                print"<a href=\"log_out.php\">Log Out</a>";
+                print" ";
+                print"<a href=\"rubric.php\">Rubric</a>";
+                print"</nav>";
+        }
+       else
+        {
+                print"<nav>";
+                print"<a href=\"sign_in.php\">Sign In</a>";
+                print" ";
+                print"<a href=\"rubric.php\">Rubric</a>";
+                print"</nav>";
+        }
 
 ?>
 
-<h1>Hello World!</h1>
+<h1>Projects</h1>
 
 <?php
-//        error_reporting(0);
-//        print"<table><tr><th>Projects</th><th>Status</th></tr>";
+        print"<table><tr><th>Projects</th><th>Status</th></tr></table>";
 
 //        $db = new PDO("sqlite:rubric.db");
 //        $sql = "SELECT * FROM proj_name ASC";
