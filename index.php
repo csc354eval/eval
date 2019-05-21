6ab7a0f132266d194bbba9f6f4260375bce41cbe
csc354eval <50844891+csc354eval@users.noreply.github.com>
2019-05-21 10:28:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -33,6 +33,29 @@
 <?php
         print"<table><tr><th>Projects</th><th>Status</th></tr></table>";
 
+
+    $serverName = "csc354eval.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "rubric", // update me
+        "Uid" => "csc354admin", // update me
+        "PWD" => "GoldenBear19" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT * FROM rubric"; 
+    //TOP 20 pc.Name as CategoryName, p.name as ProductName
+    //     FROM [SalesLT].[ProductCategory] pc
+    //    JOIN [SalesLT].[Product] p
+    //     ON pc.productcategoryid = p.productcategoryid";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
+
 //        $db = new PDO("sqlite:rubric.db");
 //        $sql = "SELECT * FROM proj_name ASC";
 //        $stmt = $db->query($sql);
